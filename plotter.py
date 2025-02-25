f29e87605a0517bda71de7705f6474ac835f5f97
Benjamin Dayres <benjamin.dayres@eviden.com>
2025-02-25 11:21:52 +0100

    Better plotting

--- plotter.py
+++ plotter.py
@@ -9,9 +9,30 @@ def plot_tops_and_bottom(df : pd.DataFrame, tops=None, bottoms=None, mode="close
 
     fig = None
     idx = df.index
+
+    points = [
+        go.Scatter(
+            x=[idx[top[1]] for top in tops],
+            y=[top[2] for top in tops], mode="markers", 
+            marker=dict(color='green', size=10), 
+            name='Tops'
+        ), go.Scatter(
+            x=[idx[bottom[1]] for bottom in bottoms],
+            y=[bottom[2] for bottom in bottoms],
+            mode="markers",
+            marker=dict(color='red', size=10),
+            name='Bottoms'
+        )
+    ]
     
     if mode == "close":
-        fig = px.line(df, x=idx, y="Close", title="Closing prices")
+        fig = go.Figure(data=[go.Scatter(
+            x=idx,
+            y=df["Close"],
+            mode="lines",
+            name="Closing prices"
+        )] + points)
+        fig.update_layout(title="Closing prices")
     elif mode == "candle":
         fig = go.Figure(data=[go.Candlestick(
             x=idx,
@@ -20,14 +41,11 @@ def plot_tops_and_bottom(df : pd.DataFrame, tops=None, bottoms=None, mode="close
             low=df['Low'],
             close=df['Close'],
             name="Prices"
-        )])
+        )] + points)
         fig.update_layout(xaxis_rangeslider_visible=False, title="Candle prices")
-        fig.update_yaxes(rangebreaks=[
-            { 'pattern': 'day of week', 'bounds': [6, 1]}
-        ])
-
-    fig.add_scatter(x=[idx[top[1]] for top in tops], y=[top[2] for top in tops], mode="markers", marker=dict(color='green', size=10), name='Tops')
-    fig.add_scatter(x=[idx[bottom[1]] for bottom in bottoms], y=[bottom[2] for bottom in bottoms], mode="markers", marker=dict(color='red', size=10), name='Bottoms')
+    fig.update_yaxes(rangebreaks=[
+        { 'pattern': 'day of week', 'bounds': [6, 1]}
+    ])
 
     return fig
 
@@ -37,11 +55,11 @@ def main():
     data["Date"] = pd.to_datetime(data["Date"])
     data = data.set_index("Date")
 
-    #tops, bottoms = ta.rolling_window(data["Close"].to_numpy(), 100)
+    tops, bottoms = ta.rolling_window(data["Close"].to_numpy(), 100)
 
-    tops, bottoms = ta.directional_change(data["Close"].to_numpy(), data["High"].to_numpy(), data["Low"].to_numpy(), 0.2)
+    # tops, bottoms = ta.directional_change(data["Close"].to_numpy(), data["High"].to_numpy(), data["Low"].to_numpy(), 0.2)
 
-    fig = plot_tops_and_bottom(data, tops, bottoms, mode="candle")
+    fig = plot_tops_and_bottom(data, tops, bottoms, mode="close")
 
     fig.show()
 
