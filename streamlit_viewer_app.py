9ce6735043e11193fda19146a37486d84d1fc825
Benjamin Dayres <benjamin.dayres@eviden.com>
2025-02-25 11:06:38 +0100

    Added customize plot style and scale options

--- streamlit_viewer_app.py
+++ streamlit_viewer_app.py
@@ -7,21 +7,6 @@ import plotly.graph_objects as go
 from plotter import plot_tops_and_bottom
 import technical_analysis as ta
 
-# def show_candles(df : pd.DataFrame, start, end, scale):
-#     interval_df = df.loc[start:end]
-#     fig = go.Figure(data=[go.Candlestick(
-#         x=interval_df.index,
-#         open=interval_df['Open'],
-#         high=interval_df['High'],
-#         low=interval_df['Low'],
-#         close=interval_df['Close']
-#     )])
-#     fig.update_layout(xaxis_rangeslider_visible=False)
-#     fig.update_yaxes(rangebreaks=[
-#         { 'pattern': 'day of week', 'bounds': [6, 1]}
-#     ], type=scale)
-#     st.plotly_chart(fig)
-
 def main():
     st.title("Stoxx600 Viewer App 💸")
 
@@ -42,7 +27,13 @@ def main():
         # end = st.date_input("End", value='today')
 
         tops, bottoms = ta.rolling_window(stock_values_df["Close"].to_numpy(), 10)
-        fig = plot_tops_and_bottom(stock_values_df, tops, bottoms)
+
+        style = st.selectbox("Plot style", ("close", "candle"), index=0)
+        scale = st.selectbox("Scale", ("lin", "log"), index=0)
+
+        fig = plot_tops_and_bottom(stock_values_df, tops, bottoms, style)
+        if scale == "log":
+            fig.update_yaxes(type="log")
         st.plotly_chart(fig)
 
 main()
