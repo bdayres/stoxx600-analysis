982dcca3d954a9d8f338978ba5832d2b840694a5
Benjamin Dayres <benjamin.dayres@eviden.com>
2025-03-07 12:13:23 +0100

    cache as resource

--- simulator.py
+++ simulator.py
@@ -3,7 +3,7 @@ import pandas as pd
 import strategies.strategy as stg
 import streamlit as st
 
-@st.cache_data
+@st.cache_resource
 def simulate(data : pd.DataFrame, strategy : stg.Strategy, lookback : int):
     for i in range(lookback, len(data)):
         strategy.make_choice(data.iloc[i])
