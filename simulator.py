847af47a1556bcb5a008369687d5f7c6d12a5a8c
Benjamin Dayres <benjamin.dayres@eviden.com>
2025-03-07 12:12:39 +0100

    cache simulation

--- simulator.py
+++ simulator.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 import strategies.strategy as stg
+import streamlit as st
 
+@st.cache_data
 def simulate(data : pd.DataFrame, strategy : stg.Strategy, lookback : int):
     for i in range(lookback, len(data)):
         strategy.make_choice(data.iloc[i])
